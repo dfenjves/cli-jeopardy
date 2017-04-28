8abd22bf2795796b54dc159293f60d95ed461f7a
dfenjves <fenjves.daniel@gmail.com>
2017-04-28 14:26:36 -0400

    fixed questions

--- game.py
+++ game.py
@@ -7,18 +7,15 @@ class Game(object):
     """docstring for Game."""
     def __init__(self):
         print "Welcome to Jeopardy!"
-        time.sleep(2)
+        time.sleep(1)
         print "What is your name?"
         self.player = Player(raw_input())
         print "Hello " + self.player.name + ", get ready to play!"
         self.board = Board()
-        self.board.display_whole_board()
-        self.prompt_and_play()
-        self.board.display_whole_board()
-        self.prompt_and_play()
-        self.board.display_whole_board()
-        self.prompt_and_play()
-        self.board.display_whole_board()
+        while self.finished_board() == False:
+            self.board.display_whole_board()
+            self.prompt_and_play()
+
 
     def prompt_and_play(self): #NEEDS WORK
         print "Please select a category from the board"
@@ -27,10 +24,14 @@ class Game(object):
         selected_value = raw_input()
         for cat in self.board.categories:
             fuzz_ratio = fuzz.ratio(cat.title, selected_category)
-            if fuzz_ratio > 0.75:
+            if cat.title == selected_category:
                 self.player.score += cat.questions[selected_value].ask()
-            else:
-                print "Unable to find that Category"
+        print self.player.name+", your score is now $" + str(self.player.score)
 
-
+    def finished_board(self):
+        for cat in self.board.categories:
+            for val in [100,200,300,400,500]:
+                if cat.questions[str(val)].unused == True:
+                    return False
+        return True
 Game()
